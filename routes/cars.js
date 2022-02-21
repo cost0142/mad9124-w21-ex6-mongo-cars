58d066d7ac81a3caf9418e3d89795a07111a4bf9
Hygor Costa <cost0142@algonquinlive.com>
2022-02-21 01:00:49 -0500

    Add the PATCH route logic

--- routes/cars.js
+++ routes/cars.js
@@ -33,7 +33,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.patch("/:id", async (req, res) => {});
+router.patch("/:id", async (req, res) => {
+  try {
+    const { _id, ...attributes } = req.body.data.attributes;
+    const car = await Car.findByIdAndUpdate(
+      req.params.id,
+      { _id: req.params.id, ...otherAttributes },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!car) {
+      throw new Error("Resource not found");
+    }
+    res.json({ data: formatResponseData("cars", car.toObject()) });
+  } catch (err) {
+    sendResourceNotFound(req, res);
+  }
+});
 
 router.put("/:id", async (req, res) => {});
 
