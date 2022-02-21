9acbca29b522b41d69de64a59b98538d2f45ce28
Hygor Costa <cost0142@algonquinlive.com>
2022-02-21 11:16:21 -0500

    add populate

--- models/Car.js
+++ models/Car.js
@@ -1,9 +1,11 @@
+const Person = require("./Person");
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   make: String,
   model: String,
   colour: { type: String },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "Person" },
 });
 const Model = mongoose.model("Car", schema);
 
