103ac664455304301f9d23f516cff18c6912ff6e
Hygor Costa <cost0142@algonquinlive.com>
2022-02-21 00:16:17 -0500

    Add the GET all cars route logic

--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const Car = require("../models/Car");
 const router = express.Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  const cars = await Car.find();
+
+  res.json({
+    data: cars.map((car) => formatResponseData("cars", car.toObject())),
+  });
+});
 
 router.post("/", async (req, res) => {});
 
@@ -12,5 +19,16 @@ router.patch("/:id", async (req, res) => {});
 router.put("/:id", async (req, res) => {});
 
 router.delete("/:id", async (req, res) => {});
+
+/**
+ * Format the response data object according to JSON:API v1.0
+ * @param {string} type The resource collection name, e.g. 'cars'
+ * @param {Object} resource An instance object from that collection
+ * @returns
+ */
+function formatResponseData(type, resource) {
+  const { _id, ...attributes } = resource;
+  return { type, id: _id, attributes };
+}
 
 module.exports = router;
